03d740c3cad7ceeee4ad46eba6ed50694ad79fee
simms <simms@simmsdeMacBook-Air.local>
2025-06-24 23:16:33 +0800

    0624

--- src/main/java/dailyPractice/hot100/LC2.java
+++ src/main/java/dailyPractice/hot100/LC2.java
@@ -17,6 +17,41 @@ import dto.ListNode;
 
 public class LC2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+          int n1 = l1 == null ? 0 : l1.val;
+          int n2 = l2 == null ? 0 : l2.val;
+          int sum = n1 + n2 + carry;
+          carry = sum / 10;
+          if(head == null) {
+              head = tail = new ListNode(sum % 10);
+          } else {
+              tail.next = new ListNode(sum % 10);
+              tail = tail.next;
+          }
+          if (l1 != null) {
+              l1 = l1.next;
+          }
+          if (l2 != null) {
+              l2 = l2.next;
+          }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+
+
+
+
+
+
+
+
+
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
         ListNode head = null, tail = null; // 头尾指针
         int carry = 0; //进位标识
         while (l1 != null || l2 != null) {
@@ -45,8 +80,8 @@ public class LC2 {
 
     public static void main(String[] args) {
         ListNode node1 = new ListNode(2);
-        ListNode node2 = new ListNode(3);
-        ListNode node3 = new ListNode(4);
+        ListNode node2 = new ListNode(4);
+        ListNode node3 = new ListNode(3);
         ListNode nodeA = new ListNode(5);
         ListNode nodeB = new ListNode(6);
         ListNode nodeC = new ListNode(4);
--- src/main/java/dto/TreeNode.java
+++ src/main/java/dto/TreeNode.java
@@ -1,9 +1,38 @@
 package dto;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
      public int val;
      public TreeNode left;
      public TreeNode right;
      public TreeNode(int x) { val = x; }
+    public TreeNode buildTree(int[] nodes) {
+        if (nodes == null || nodes.length == 0 || nodes[0] == Integer.MIN_VALUE) {
+            return null;
+        }
 
+        TreeNode root = new TreeNode(nodes[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int i = 1;
+        while (i < nodes.length) {
+            TreeNode current = queue.poll();
+
+            if (nodes[i] != Integer.MIN_VALUE) {
+                current.left = new TreeNode(nodes[i]);
+                queue.add(current.left);
+            }
+            i++;
+
+            if (i < nodes.length && nodes[i] != Integer.MIN_VALUE) {
+                current.right = new TreeNode(nodes[i]);
+                queue.add(current.right);
+            }
+            i++;
+        }
+        return root;
+    }
   }
